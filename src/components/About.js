58748e65d7b7edb5d028271745872c0326cedfdd
Diana Jordan <dianacjordan@gmail.com>
2023-03-15 17:05:51 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+
+function About(props) {
+
+  // function Bio() {
+  //   if (props.bio && props.bio.trim().length > 1) {
+  //     return <p>{props.bio}</p>
+  //   } else {
+  //     return null
+  //   }
+  // }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <Bio  /> */}
+      {props.bio && props.bio.trim().length > 1 ? <p>{props.bio}</p> : null}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+    <Links github={props.links.github} linkedin={props.links.linkedin}/> 
     </div>
   );
+
 }
 
 export default About;
